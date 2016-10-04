01a030d23dd0abae68fe9413c994ff5ca675dd7f
DrewK1 <dkorven1@studentmail.nait.ca>
2016-10-04 15:40:30 -0600

    added new classes

---
+++ Chinook Demo/Chinook.Framework/BLL/CustomerManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chinook.Framework.DAL; // for the chinookcontext class
+using Chinook.Framework.Entities; // for the database entities
+using Chinook.Framework.Entities.ViewModels; // for my DTOs/POCOs, etc.
+
+namespace Chinook.Framework.BLL
+{
+    public class CustomerManager
+    {
+
+    }
+}
